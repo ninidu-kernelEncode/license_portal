67318d1d1eba8c23f2c3083b95b7d5f5592c0f29
ninidu-kernelEncode <ninidu@kernelencode.com>
2025-07-24 18:20:14 +0530

    Fix migration file issue

--- database/migrations/2025_07_21_110258_create_products_table.php
+++ database/migrations/2025_07_21_110258_create_products_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id('product_id');
+            $table->string('product_ref_id', 255)->unique()->after('name');
             $table->string('name');
             $table->string('version');
             $table->text('description')->nullable();
